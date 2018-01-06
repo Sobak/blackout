808f22410fc5b0e265e44c18982a6c314f7f47d7
Sobak <msobaczewski@gmail.com>
2018-01-06 10:02:41 +0100

    Translate Contact screen

--- language/en/contact.php
+++ language/en/contact.php
@@ -1,9 +1,7 @@
 <?php
 
 $lang['ctc_title'] = "Contact";
-$lang['ctc_intro'] = "Ici vous trouverez les adresses pour contacter les op&eacute;rateurs";
-$lang['ctc_name']  = "Nom";
-$lang['ctc_rank']  = "Rang";
-$lang['ctc_mail']  = "eMail";
-
-?>
+$lang['ctc_intro'] = "Here you will find the addresses to contact the operators";
+$lang['ctc_name']  = "Username";
+$lang['ctc_rank']  = "Rank";
+$lang['ctc_mail']  = "E-mail";
