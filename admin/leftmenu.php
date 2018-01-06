a0fc49b22956c64c9b6f9bd0179d95f76a2034c7
Sobak <msobaczewski@gmail.com>
2018-01-06 21:16:48 +0100

    Refactor left menus PHP

--- admin/leftmenu.php
+++ admin/leftmenu.php
@@ -1,14 +1,6 @@
-<?PHP
-
-/**
- * leftmenu.php
- *
- * @version 1.0
- * @copyright 2008 by ??????? for XNova
- */
+<?php
 
 define('INSIDE'  , true);
-define('INSTALL' , false);
 define('IN_ADMIN', true);
 
 $ugamela_root_path = './../';
@@ -16,33 +8,23 @@ include($ugamela_root_path . 'common.php');
 
 includeLang('leftmenu');
 
-    if ($user['authlevel'] == "1") {
-      $parse                 = $lang;
-      $parse['mf']           = "Hauptframe";
-      $parse['dpath']        = $dpath;
-      $parse['XNovaRelease'] = VERSION;
-      $parse['servername']   = XNova;
-      $Page                  = parsetemplate(gettemplate('admin/left_menu_modo'), $parse);
-      display( $Page, "", false, '', true);
-   }
-    elseif ($user['authlevel'] == "2") {
-      $parse                 = $lang;
-      $parse['mf']           = "Hauptframe";
-      $parse['dpath']        = $dpath;
-      $parse['XNovaRelease'] = VERSION;
-      $parse['servername']   = XNova;
-      $Page                  = parsetemplate(gettemplate('admin/left_menu_op'), $parse);
-      display( $Page, "", false, '', true);
-   }
-    elseif ($user['authlevel'] >= "3") {
-      $parse                 = $lang;
-      $parse['mf']           = "Hauptframe";
-      $parse['dpath']        = $dpath;
-      $parse['XNovaRelease'] = VERSION;
-      $parse['servername']   = XNova;
-      $Page                  = parsetemplate(gettemplate('admin/left_menu'), $parse);
-      display( $Page, "", false, '', true);
-    } else {
-        message( $lang['sys_noalloaw'], $lang['sys_noaccess'] );
-    }
-?>
+if ($user['authlevel'] < 1) {
+    message($lang['sys_noalloaw'], $lang['sys_noaccess']);
+}
+
+$parse                 = $lang;
+$parse['mf']           = "Hauptframe";
+$parse['dpath']        = $dpath;
+$parse['XNovaRelease'] = VERSION;
+$parse['servername']   = 'XNova';
+
+if ($user['authlevel'] == 1) {
+    $template = 'admin/left_menu_modo';
+} elseif ($user['authlevel'] == 2) {
+    $template = 'admin/left_menu_op';
+} elseif ($user['authlevel'] >= 3) {
+    $template = 'admin/left_menu';
+}
+
+$page = parsetemplate(gettemplate($template), $parse);
+display($page, '', false, '', true);
--- leftmenu.php
+++ leftmenu.php
@@ -1,56 +1,33 @@
-<?PHP
-
-/**
- * leftmenu.php
- *
- * @version 1.1
- * @copyright 2008 By Chlorel for XNova
- */
+<?php
 
 define('INSIDE'  , true);
-define('INSTALL' , false);
 
 $ugamela_root_path = './';
 include($ugamela_root_path . 'common.php');
 
-function ShowLeftMenu ( $Level , $Template = 'left_menu') {
-    global $lang, $dpath, $game_config;
+includeLang('leftmenu');
 
-    includeLang('leftmenu');
-
-    $MenuTPL                  = gettemplate( $Template );
-    $InfoTPL                  = gettemplate( 'serv_infos' );
-    $parse                    = $lang;
-    $parse['lm_tx_serv']      = $game_config['resource_multiplier'];
-    $parse['lm_tx_game']      = $game_config['game_speed'] / 2500;
-    $parse['lm_tx_fleet']     = $game_config['fleet_speed'] / 2500;
-    $parse['lm_tx_queue']     = MAX_FLEET_OR_DEFS_PER_ROW;
-    $SubFrame                 = parsetemplate( $InfoTPL, $parse );
-    $parse['server_info']     = $SubFrame;
-    $parse['XNovaRelease']    = VERSION;
-    $parse['dpath']           = $dpath;
-    $parse['forum_url']       = $game_config['forum_url'];
-    $parse['mf']              = "Hauptframe";
-    $rank                     = doquery("SELECT `total_rank` FROM {{table}} WHERE `stat_code` = '1' AND `stat_type` = '1' AND `id_owner` = '". $user['id'] ."';",'statpoints',true);
-    $parse['user_rank']       = $rank['total_rank'];
-    if ($Level > 0) {
-        $parse['ADMIN_LINK']  = "
-        <tr>
-            <td colspan=\"2\"><div><a href=\"admin/leftmenu.php\"><font color=\"lime\">".$lang['user_level'][$Level]."</font></a></div></td>
-        </tr>";
-    } else {
-        $parse['ADMIN_LINK']  = "";
-    }
-    $parse['servername']   = $game_config['game_name'];
-    $Menu                  = parsetemplate( $MenuTPL, $parse);
-
-    return $Menu;
+$parse                    = $lang;
+$parse['lm_tx_serv']      = $game_config['resource_multiplier'];
+$parse['lm_tx_game']      = $game_config['game_speed'] / 2500;
+$parse['lm_tx_fleet']     = $game_config['fleet_speed'] / 2500;
+$parse['lm_tx_queue']     = MAX_FLEET_OR_DEFS_PER_ROW;
+$parse['server_info']     = parsetemplate(gettemplate('serv_infos'), $parse);
+$parse['XNovaRelease']    = VERSION;
+$parse['dpath']           = $dpath;
+$parse['forum_url']       = $game_config['forum_url'];
+$parse['mf']              = "Hauptframe";
+$rank                     = doquery("SELECT `total_rank` FROM {{table}} WHERE `stat_code` = '1' AND `stat_type` = '1' AND `id_owner` = '". $user['id'] ."';",'statpoints',true);
+$parse['user_rank']       = $rank['total_rank'];
+if ($user['authlevel'] > 0) {
+    $parse['ADMIN_LINK']  = '
+    <tr>
+        <td colspan="2"><a href="admin/leftmenu.php" style="color:lime">'.$lang['user_level'][$user['authlevel']].'</a></td>
+    </tr>';
+} else {
+    $parse['ADMIN_LINK']  = '';
 }
-    $Menu = ShowLeftMenu ( $user['authlevel'] );
-    display ( $Menu, "Menu", '', false );
+$parse['servername']   = $game_config['game_name'];
+$Menu                  = parsetemplate(gettemplate('left_menu'), $parse);
 
-// -----------------------------------------------------------------------------------------------------------
-// History version
-// 1.0 - Passage en fonction pour XNova version future
-// 1.1 - Modification pour gestion Admin / Game OP / Modo
-?>
+display ( $Menu, "Menu", '', false );
