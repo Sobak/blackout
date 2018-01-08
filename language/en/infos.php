53d6d1a052fe456173c1cce319a95ce6acd504af
Sobak <msobaczewski@gmail.com>
2018-01-08 18:39:18 +0100

    Translate Info screen interface

--- language/en/infos.php
+++ language/en/infos.php
@@ -2,28 +2,28 @@
 
 // Interface
 $lang['nfo_page_title']  = "Information";
-$lang['nfo_title_head']  = "Information concernant";
-$lang['nfo_name']        = "Nom";
-$lang['nfo_destroy']     = "D&eacute;truire";
-$lang['nfo_level']       = "Niveau";
-$lang['nfo_range']       = "Port&eacute;e des capteurs";
-$lang['nfo_used_energy'] = "Consommation d'&eacute;nergie";
-$lang['nfo_used_deuter'] = "Consommation de Deut&eacute;rium";
-$lang['nfo_prod_energy'] = "Production d'&eacute;nergie";
-$lang['nfo_difference']  = "Diff&eacute;rence";
-$lang['nfo_prod_p_hour'] = "Production/heure";
-$lang['nfo_needed']      = "N&eacute;cessite";
-$lang['nfo_dest_durati'] = "Dur&eacute;e de destruction";
+$lang['nfo_title_head']  = "Information about the";
+$lang['nfo_name']        = "Name";
+$lang['nfo_destroy']     = "Destroy";
+$lang['nfo_level']       = "Level";
+$lang['nfo_range']       = "Range";
+$lang['nfo_used_energy'] = "Energy consumption";
+$lang['nfo_used_deuter'] = "Deuterium consumption";
+$lang['nfo_prod_energy'] = "Energy production";
+$lang['nfo_difference']  = "Difference";
+$lang['nfo_prod_p_hour'] = "Production/hour";
+$lang['nfo_needed']      = "Required";
+$lang['nfo_dest_durati'] = "Time of descruction";
 
-$lang['nfo_struct_pt']   = "Points de structure";
-$lang['nfo_shielf_pt']   = "Puissance du bouclier";
-$lang['nfo_attack_pt']   = "Valeur d'attaque";
-$lang['nfo_rf_again']    = "Feu rapide contre";
-$lang['nfo_rf_from']     = "Feu rapide de";
-$lang['nfo_capacity']    = "Capacit&eacute; de fret";
-$lang['nfo_units']       = "Unit&eacute;s";
-$lang['nfo_base_speed']  = "Vitesse de base";
-$lang['nfo_consumption'] = "Consommation de carburant (Deut&eacute;rium)";
+$lang['nfo_struct_pt']   = "Structural points";
+$lang['nfo_shielf_pt']   = "Defense points";
+$lang['nfo_attack_pt']   = "Attack points";
+$lang['nfo_rf_again']    = "Fast fire against";
+$lang['nfo_rf_from']     = "Fast fire";
+$lang['nfo_capacity']    = "Cargo capacity";
+$lang['nfo_units']       = "units";
+$lang['nfo_base_speed']  = "Base speed";
+$lang['nfo_consumption'] = "Deuterium consumption";
 
 // Jumpgate interface
 $lang['gate_start_moon'] = "Lune de d&eacute;part";
