06b4748dcda879ffc2722d35dff721ce52899d3d
Sobak <msobaczewski@gmail.com>
2018-01-09 19:54:57 +0100

    Improve logout experience

--- language/en/logout.php
+++ language/en/logout.php
@@ -1,16 +1,4 @@
 <?php
 
-/*******************************************************************************************************
-*
-*        german UGamela
-*        http://ugamela-forum.pheelgood.net
-*        UGamela basescripts from Perberos @2006 (all rights reversed)
-*        
-*
-*******************************************************************************************************/
-
-
-$lang['see_you'] = 'See you again your GG Team..... ';
-$lang['session_closed'] = "Session is Closed";
-
-?>
+$lang['see_you'] = 'See you again soon!';
+$lang['session_closed'] = "Logout successful";
--- logout.php
+++ logout.php
@@ -1,23 +1,12 @@
 <?php
 
-/**
- * logout.php
- *
- * @version 1.0
- * @copyright 2008 by ?????? for XNova
- */
-
 define('INSIDE'  , true);
 
 $ugamela_root_path = './';
 include($ugamela_root_path . 'common.php');
 
-    includeLang('logout');
+includeLang('logout');
 
-    setcookie($game_config['COOKIE_NAME'], "", time()-100000, "/", "", 0);
+setcookie($game_config['COOKIE_NAME'], "", time()-100000, "/", "", 0);
 
-    message ( $lang['see_you'], $lang['session_closed'], "login.php" );
-
-// -----------------------------------------------------------------------------------------------------------
-// History version
-?>
+message($lang['see_you'], $lang['session_closed'], "login.php", 2);
