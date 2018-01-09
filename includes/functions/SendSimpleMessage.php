ab833e7507aa269a3561a58fafd193e95cfb24da
Sobak <msobaczewski@gmail.com>
2018-01-09 08:11:10 +0100

    Messages: refactor SendSimpleMessage()

--- includes/functions/SendSimpleMessage.php
+++ includes/functions/SendSimpleMessage.php
@@ -1,23 +1,18 @@
 <?php
 
 /**
- * SendSimpleMessage.php
+ * Sends private message to the user.
  *
- * @version 1.3
- * @copyright 2008 by Chlorel for XNova
+ * @param int $Owner ID of recipient
+ * @param int $Sender ID of message sender
+ * @param int $Time Timestamp of message
+ * @param int $Type Message type
+ * @param string $From Sender display name
+ * @param string $Subject Message subject
+ * @param string $Message Message contents
  */
-
-// Envoi d'un message simple
-//
-// $Owner   -> destinataire
-// $Sender  -> ID de l'emeteur
-// $Time    -> Heure theorique a laquelle l'evenement s'est produit
-// $Type    -> Type de message (pour classement dans les onglets message plus tard)
-// $From    -> Description de l'emeteur
-// $Subject -> Sujet
-// $Message -> Le message lui meme !!
-//
-function SendSimpleMessage ( $Owner, $Sender, $Time, $Type, $From, $Subject, $Message) {
+function SendSimpleMessage($Owner, $Sender, $Time, $Type, $From, $Subject, $Message)
+{
     global $messfields;
 
     if ($Time == '') {
@@ -32,20 +27,12 @@ function SendSimpleMessage ( $Owner, $Sender, $Time, $Type, $From, $Subject, $Me
     $QryInsertMessage .= "`message_from` = '". addslashes( $From ) ."', ";
     $QryInsertMessage .= "`message_subject` = '". addslashes( $Subject ) ."', ";
     $QryInsertMessage .= "`message_text` = '". addslashes( $Message ) ."';";
-    doquery( $QryInsertMessage, 'messages');
+    doquery($QryInsertMessage, 'messages');
 
     $QryUpdateUser  = "UPDATE {{table}} SET ";
     $QryUpdateUser .= "`".$messfields[$Type]."` = `".$messfields[$Type]."` + 1, ";
     $QryUpdateUser .= "`".$messfields[100]."` = `".$messfields[100]."` + 1 ";
     $QryUpdateUser .= "WHERE ";
     $QryUpdateUser .= "`id` = '". $Owner ."';";
-    doquery( $QryUpdateUser, 'users');
-
+    doquery($QryUpdateUser, 'users');
 }
-
-// Revision history :
-// 1.0 - Initial release (mise en fonction generique)
-// 1.1 - Ajout gestion des messages par type pour le module de messages
-// 1.2 - Correction bug (addslashes pour les zone texte pouvant contenir une apostrophe)
-// 1.3 - Correction bug (integration de la variable $Time pour afficher l'heure exacte de l'evenement pour les flottes)
-?>
