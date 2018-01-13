8f5aaf501e913f0343fc6e8c2d50300ee62f8168
Sobak <msobaczewski@gmail.com>
2018-01-13 14:38:48 +0100

    Install Laravel alongside XNova

---
+++ legacy/config.php
@@ -0,0 +1,11 @@
+<?php
+defined('INSIDE') or die;
+
+$dbsettings = [
+	'server'     => 'localhost', // MySQL server name.
+	'user'       => 'root', // MySQL username.
+	'pass'       => '', // MySQL password.
+	'name'       => 'xnova', // MySQL database name.
+	'prefix'     => 'game_', // Tables prefix.
+	'secretword' => 'XNova1168243769', // Cookies.
+];
